26e774280fad458f4fc62b3b44d8d2d78a2a2bb8
Michael <micke.green@gmail.com>
2015-02-22 18:21:46 +0100

    images and stuff.

---
+++ big-data.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Green Analysts - Visualizing the future</title>
+    <link rel="stylesheet" href="stylesheets/app.css" />
+
+    <script src="bower_components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
+
+    <?php include 'pages/header.php';?>
+      <section class="main-container text-center clearfix">
+
+        <main>
+        <h1>Big data consulting</h1>
+        <div class="row">
+        If your business is approaching or have already started dealing with big data we can help guide you through this jungle. Big data is not only a new buzz word but a reality that we all have to deal with sooner or later. Data are piling up every day and a lot of it is too valuable to throw away. Thus your business will have to find ways to visualize and extract useful insights and facts from huge amounts of data that are intractable to load for a computer. Even a fast one. We have developed a new big data frame work that allows us to capture your business’ specific needs and knowledge flow. In effect, we can in real-time display learnings from your big data setup.
+        </div>
+        <img src="images/big-data.jpg" width="400">
+        </main>
+      </section>
+
+      <?php include 'pages/footer.php';?>
+
+  </body>
+</html>
---
+++ pricing-strategies.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Green Analysts - Visualizing the future</title>
+    <link rel="stylesheet" href="stylesheets/app.css" />
+
+    <script src="bower_components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
+
+    <?php include 'pages/header.php';?>
+      <section class="main-container text-center clearfix">
+
+        <main>
+        <h1>Pricing strategies</h1>
+        <div class="row">
+        Setting the right price for your products should be determined by what consumers are willing to pay for it. Consequently, a simple price = cost + profit is most likely sub-optimal and should be revised. Continuously and consistently measuring and quantifying the effect of price changes on your sales provides an unbiased and objective estimate of your price elasticity.
+        Price elasticity changes over time depending on, among other factors, seasonality, distribution, competitor activities, weather and brand strength. Therefor, the right price is also changing over time which means your business should try to capture this market dynamics to optimize profit.
+        </div>
+        <img src="images/right-price.png" width="400">
+        </main>
+      </section>
+
+      <?php include 'pages/footer.php';?>
+
+  </body>
+</html>
