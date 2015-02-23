dae5c10a0d71d6662d91c9923d4c0f736359fdc9
Michael <micke.green@gmail.com>
2015-02-23 20:40:45 +0100

    Added more content.

--- big-data.php
+++ big-data.php
@@ -16,7 +16,9 @@
         <main>
         <h1>Big data consulting</h1>
         <div class="row">
+        <p class="text-left">
         If your business is approaching or have already started dealing with big data we can help guide you through this jungle. Big data is not only a new buzz word but a reality that we all have to deal with sooner or later. Data are piling up every day and a lot of it is too valuable to throw away. Thus your business will have to find ways to visualize and extract useful insights and facts from huge amounts of data that are intractable to load for a computer. Even a fast one. We have developed a new big data frame work that allows us to capture your business’ specific needs and knowledge flow. In effect, we can in real-time display learnings from your big data setup.
+        </p>
         </div>
         <img src="images/svg/bigdata.svg" width="300">
         </main>
