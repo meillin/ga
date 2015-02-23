d88d944613d050bd8dbd25587e705346e02cd7db
Meilan Lin <meilan.dk@gmail.com>
2015-02-23 20:48:52 +0100

    Move all the header info to header.php

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!doctype html>
-<html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Green Analysts - Visualizing the future</title>
-    <link rel="stylesheet" href="stylesheets/app.css" />
-    <script src="bower_components/modernizr/modernizr.js"></script>
-  </head>
-  <body>
-
       <?php include 'pages/header.php';?>
 
       <section class="video-container text-center clearfix show-for-medium-up">
--- pages/header.php
+++ pages/header.php
@@ -1,5 +1,16 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Green Analysts - Visualizing the future</title>
+    <link rel="stylesheet" href="stylesheets/app.css" />
 
-<div class="off-canvas-wrap" data-offcanvas>
+    <script src="bower_components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
+
+    <div class="off-canvas-wrap" data-offcanvas>
   <div class="inner-wrap">
 
     <!-- main content goes here -->
