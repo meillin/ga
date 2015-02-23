c8d7650e5817c05f1be6c9cfbbc798f7dd977d95
Michael <micke.green@gmail.com>
2015-02-23 21:46:34 +0100

    Added meta tags to big data and pricing file.

--- pricing-strategies.php
+++ pricing-strategies.php
@@ -1,12 +1,36 @@
 <!doctype html>
 <html class="no-js" lang="en">
   <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="utf-8" >
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
     <title>Green Analysts - Visualizing the future</title>
-    <link rel="stylesheet" href="stylesheets/app.css" />
-    <script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+
+    <link rel="canonical" href="greenanalysts.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="keywords" content="green analysts, roi modeling, machine learning, artificial intelligence, fact based marketing, price optimization, pricing strategies, marketing mix modeling" >
+    <meta name="description" content="Green Analysts is a machine learning consulting company specializing in building, customizing and integrating predictive analytics solutions in our clients' decision making process. In short, we help companies identify, quantify and optimize all kinds of revenue generating activities.">
+    <!--FACEBOOK-->
+    <meta property="og:title" content="Finding the optimal price for your product" >
+    <meta property="og:site_name" content="Green Analysts">
+    <meta property="og:url" content="greenanalysts.com" >
+    <meta property="og:description" content="Green Analysts is a machine learning consulting company specializing in building, customizing and integrating predictive analytics solutions in our clients' decision making process. In short, we help companies identify, quantify and optimize all kinds of revenue generating activities." >
+    <meta property="og:image" content="http://greenanalysts.com/images/price.svg" >
+    <meta property="og:type" content="website" >
+    <meta property="og:locale" content="en-US" >
+    <!--TWITTER-->
+    <meta property="twitter:card" content="summary" >
+    <meta property="twitter:title" content="Finding the optimal price for your product" >
+    <meta property="twitter:description" content="Green Analysts is a machine learning consulting company specializing in building, customizing and integrating predictive analytics solutions in our clients' decision making process. In short, we help companies identify, quantify and optimize all kinds of revenue generating activities." >
+    <meta property="twitter:creator" content="@DoktorMike" >
+    <meta property="twitter:url" content="greenanalysts.com" >
+    <meta property="twitter:image" content="http://greenanalysts.com/images/price.svg" >
+    <!--GOOGLE+-->
+    <link rel="author" href="https://www.google.com/+MichaelGreenSolosEtUnicus">    
+    <link rel="publisher" href="https://www.google.com/+GreenanalystsDK">
+    
+    <link rel="stylesheet" href="stylesheets/app.css" >
     <script src="bower_components/modernizr/modernizr.js"></script>
+    <script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
   </head>
   <body>
 
