80fb9ff9ee7103005799e1334b9bb31698af4aa0
Michael <micke.green@gmail.com>
2015-03-14 19:03:02 +0100

    CRM content.

---
+++ crm.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" >
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+    <title>Green Analysts - Data driven and predictive CRM</title>
+    <link rel="shortcut icon" href="images/favicon.ico" />
+    <link rel="canonical" href="greenanalysts.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="keywords" content="green analysts, roi modeling, machine learning, artificial intelligence, fact based marketing, price optimization, pricing strategies, marketing mix modeling" >
+    <meta name="description" content="Green Analysts is a machine learning consulting company specializing in building, customizing and integrating predictive analytics solutions in our clients' decision making process. In short, we help companies identify, quantify and optimize all kinds of revenue generating activities.">
+    <!--FACEBOOK-->
+    <meta property="og:title" content="Putting all your big data to good use" >
+    <meta property="og:site_name" content="Green Analysts">
+    <meta property="og:url" content="greenanalysts.com" >
+    <meta property="og:description" content="Green Analysts is a machine learning consulting company specializing in building, customizing and integrating predictive analytics solutions in our clients' decision making process. In short, we help companies identify, quantify and optimize all kinds of revenue generating activities." >
+    <meta property="og:image" content="http://greenanalysts.com/images/svg/bigdata.svg" >
+    <meta property="fb:app_id" content="705802480" >
+    <meta property="og:type" content="website" >
+    <meta property="og:locale" content="en-US" >
+    <!--TWITTER-->
+    <meta property="twitter:card" content="summary" >
+    <meta property="twitter:title" content="Putting all your big data to good use" >
+    <meta property="twitter:description" content="Green Analysts is a machine learning consulting company specializing in building, customizing and integrating predictive analytics solutions in our clients' decision making process. In short, we help companies identify, quantify and optimize all kinds of revenue generating activities." >
+    <meta property="twitter:creator" content="@DoktorMike" >
+    <meta property="twitter:url" content="greenanalysts.com" >
+    <meta property="twitter:image" content="http://greenanalysts.com/images/svg/bigdata.svg" >
+    <!--GOOGLE+-->
+    <link rel="author" href="https://www.google.com/+MichaelGreenSolosEtUnicus">
+    <link rel="publisher" href="https://www.google.com/+GreenanalystsDK">
+    
+    <link rel="stylesheet" href="stylesheets/app.css" >
+    <script src="bower_components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
+
+    <?php include 'pages/header.php';?>
+      <section class="main-container text-center clearfix">
+
+        <main>
+        <h1>Data driven and predictive customer relationship management</h1>
+        <div class="row">
+            <div class="large-12 columns">
+                <p class="text-left">
+                There are many technologies, software solutions and processes that deals with customer interaction. This interaction is important for past, current and future customers alike. Depending on where in the life-cycle a given customer is located they are triggered by different messages. Figuring out which kind of message should go to which customer can be discovered and quantified through predictive analytics.
+                </p>
+            </div>
+        </div>
+        <img src="images/svg/bigdata.svg" width="300">
+        </main>
+      </section>
+
+      <?php include 'pages/footer.php';?>
+
+  </body>
+</html>
