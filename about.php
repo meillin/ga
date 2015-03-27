daaef36d8b8903b50f515b9b528a3f66a3870127
Meilan Lin <meilan.dk@gmail.com>
2015-03-27 15:37:56 +0100

    Add cute icons to about page

--- about.php
+++ about.php
@@ -1,80 +1,80 @@
 <!doctype html>
 <html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Green Analysts - Visualizing the future</title>
-    <link rel="shortcut icon" href="images/favicon.ico" />
-    <link rel="stylesheet" href="stylesheets/app.css" />
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Green Analysts - Visualizing the future</title>
+		<link rel="shortcut icon" href="images/favicon.ico" />
+		<link rel="stylesheet" href="stylesheets/app.css" />
 
-    <script src="bower_components/modernizr/modernizr.js"></script>
-  </head>
-  <body>
+		<script src="bower_components/modernizr/modernizr.js"></script>
+	</head>
+	<body>
 
-      <?php include 'pages/header.php';?>
+			<?php include 'pages/header.php';?>
 
-      <section class="main-container clearfix">
+			<section class="main-container clearfix">
 
-        <main class="page-about">
-          <h1>Who we are</h1>
-          <h2>We love artificial intelligence and machine learning</h2>
-          <h2 class="subheader">applied to business challenges</h2>
-          <hr />
+				<main class="page-about">
+					<h1>Who we are</h1>
+					<h2>We love artificial intelligence and machine learning</h2>
+					<h2 class="subheader">applied to business challenges</h2>
+					<hr />
 
-          <div class="row">
-            <div class="large-12 columns">
-              <p>
-                Green Analysts is a machine learning consulting company specializing in building, customizing and integrating predictive analytics solutions in our clients' decision making process. In short, we help companies identify, quantify and optimize all kinds of revenue generating activities.
-              </p>
-              <p>
-                We provide solutions to tough business challenges in many different sectors. By tough we don't mean uncommon, rather, we're referring to challenges that can only be solved by applying state of the art statistical algorithms.
-              </p>
-              <p>
-                Our small team consists of dedicated people who will go the extra mile to provide you with a top notch service.
-              </p>
-            </div>
-          </div>
+					<div class="row">
+						<div class="large-12 columns">
+							<p>
+								Green Analysts is a machine learning consulting company specializing in building, customizing and integrating predictive analytics solutions in our clients' decision making process. In short, we help companies identify, quantify and optimize all kinds of revenue generating activities.
+							</p>
+							<p>
+								We provide solutions to tough business challenges in many different sectors. By tough we don't mean uncommon, rather, we're referring to challenges that can only be solved by applying state of the art statistical algorithms.
+							</p>
+							<p>
+								Our small team consists of dedicated people who will go the extra mile to provide you with a top notch service.
+							</p>
+						</div>
+					</div>
 
-          <div class="row company-members">
-            <div class="large-6 columns">
-              <div class="small-4 columns michael-green"><img src="images/michael-green.jpg"></div>
-              <div class="small-8 columns">
-                <h5>Dr. Michael Green - Founder</h5>
-                Michael is our commercial guy and has worked with clients across multiple sectors. With a background in Theoretical Physics Michael combines business knowledge with state of the art predictive analytics. He comes from a position as CTO at Data Intelligence.
-              </div>
-            </div>
+					<div class="row company-members">
+						<div class="large-6 columns">
+							<div class="small-4 columns michael-green"><img src="images/michael-green.jpg"></div>
+							<div class="small-8 columns">
+								<h3><span>&#9996;</span>Dr. Michael Green - Founder</h3>
+								Michael is our commercial guy and has worked with clients across multiple sectors. With a background in Theoretical Physics Michael combines business knowledge with state of the art predictive analytics. He comes from a position as CTO at Data Intelligence.
+							</div>
+						</div>
 
-            <div class="large-6 columns">
-              <div class="small-4 columns"><img src="images/li-wen.jpg"></div>
-              <div class="small-8 columns">
-                <h5>Dr. Liwen You - Scientific advisor and co-founder</h5>
-                Liwen is a leading expert on econometric modeling in Sweden. She has many years of hands-on experience with implementing predictive models within several industries including telecom, life-science and traveling.
-                Liwen also holds a PhD in Machine Learning.
-              </div>
-            </div>
+						<div class="large-6 columns">
+							<div class="small-4 columns"><img src="images/li-wen.jpg"></div>
+							<div class="small-8 columns">
+								<h3><span>&#9829;</span>Dr. Liwen You - Scientific advisor and co-founder</h3>
+								Liwen is a leading expert on econometric modeling in Sweden. She has many years of hands-on experience with implementing predictive models within several industries including telecom, life-science and traveling.
+								Liwen also holds a PhD in Machine Learning.
+							</div>
+						</div>
 
-            <div class="large-6 columns">
-              <div class="small-4 columns"><img src="images/meilan-lin.jpg"></div>
-              <div class="small-8 columns">
-                <h5>Meilan Lin - Web Developer</h5>
-                Meilan is the guru coding up the stuff connecting our intelligent solutions to our clients via web services and cloud based applications.
-              </div>
-            </div>
+						<div class="large-6 columns">
+							<div class="small-4 columns"><img src="images/meilan-lin.jpg"></div>
+							<div class="small-8 columns">
+								<h3><span>&#9874;</span>Meilan Lin - Web Developer</h3>
+								Meilan is the guru coding up the stuff connecting our intelligent solutions to our clients via web services and cloud based applications.
+							</div>
+						</div>
 
-            <div class="large-6 columns">
-              <div class="small-4 columns"><img src="images/par-gustafsson.jpg"></div>
-              <div class="small-8 columns">
-                <h5>Pär Gustavsson - CFO</h5>
-                Pär is the man who's cooking the books. He has a long experience in running the financials of small to medium sized companies leveraging expert knowledge with an amazing sense of humor.
-              </div>
-            </div>
+						<div class="large-6 columns">
+							<div class="small-4 columns"><img src="images/par-gustafsson.jpg"></div>
+							<div class="small-8 columns">
+								<h3><span class="u1f400">&#9997;</span>Pär Gustavsson - CFO</h3>
+								Pär is the man who's cooking the books. He has a long experience in running the financials of small to medium sized companies leveraging expert knowledge with an amazing sense of humor.
+							</div>
+						</div>
 
-          </div>
+					</div>
 
-        </main>
-      </section>
+				</main>
+			</section>
 
-      <?php include 'pages/footer.php';?>
+			<?php include 'pages/footer.php';?>
 
-  </body>
+	</body>
 </html>
