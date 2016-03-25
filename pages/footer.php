bfbae6e62a839d9922883303dec43591b6bf8bc6
Michael <micke.green@gmail.com>
2016-03-25 15:26:29 +0100

    Updated google analytics tracking code.

--- pages/footer.php
+++ pages/footer.php
@@ -84,7 +84,9 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-48204853-1', 'auto');
+  ga('create', 'UA-48204853-1', 'auto', {'allowLinker': true});
+  ga('require', 'linker');
+  ga('linker:autoLink', ['www.greenanalysts.com', 'greenanalysts.se', 'www.greenanalysts.se', 'profit.green', 'modeling.green', 'predictive-analytics.green'] );
   ga('send', 'pageview');
 
 </script>
